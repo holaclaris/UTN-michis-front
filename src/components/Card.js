f0107085d8ec57cf7659a7cec75ebf11af2129f3
holaclaris <mamutchiquitito@gmail.com>
2024-06-05 12:47:59 -0300

    Cambios cards gatos y Form

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,21 @@
 import "./Card.css";
+import {Fragment} from 'react';
 
 const Card = ({ gatos }) =>{
     return(
-       <div>
+       <>
           {
             gatos.map((gato)=>(
                 <div key ={gato.id} className="tarjetaGato">
                     <div>
                         <img src={gato.img} alt={gato.nombre} />
-                        <div>
+                        <div className="textoBotonesGato">
+                            <div className="textoGato">
                             <h3>{gato.nombre}</h3>
                             <p>Sexo: {gato.sexo}</p>
                             <p>Edad: {gato.edad}</p>
                             <p>Acerca de: {gato.descripcion}</p>
+                            </div>
                             <button>Eliminar</button><button>Editar</button>
                         </div>
                         
@@ -20,7 +23,7 @@ const Card = ({ gatos }) =>{
                 </div>
             ))
           }
-       </div>
+       </>
 
     )
 }
--- src/components/GatosCards.js
+++ src/components/GatosCards.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import './GatosCards.css'
 import Card from './Card';
 
 function GatosCards(){
