0319e3f62c7843168718312c6ac1c086dc4be98a
holaclaris <mamutchiquitito@gmail.com>
2024-06-13 15:27:13 -0300

    Delete agregado

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,35 @@
 import "./Card.css";
+import { useState } from "react";
 
 const Card = ({ gatos }) => {
+    const[mensajeEditado, setMensajeEditado] =useState(false)
+    const[mensajeEliminado, setMensajeEliminado] = useState(false)
+
+    //FALTA LA FUNCION PARA EDITAR 
+    //const handleEditar=(e)=>{
+    //     e.preventDefault()
+
+    // }
+
+    const handleEliminar=(e)=>{
+      e.preventDefault()
+
+      fetch(`http://localhost:4000/${e.target.id}`, {
+        method:'delete',
+        })
+        .then((resp) =>{return resp.json()})
+        .then((data) => {
+              data.status === 200 ? setMensajeEliminado(true) : setMensajeEliminado(false);
+              setTimeout(() => {setMensajeEliminado(false) }, 3000)
+        })
+        .catch(err => console.log("No se logro enviar: " + err))
+
+    }
 
     return (
+      <>  
+        { mensajeEliminado === false 
+        ? 
         <>
             {
                 gatos.map((gato) => (
@@ -17,15 +44,26 @@ const Card = ({ gatos }) => {
                                     <p>Edad: {gato.edad}</p>
                                     <p>Acerca de: {gato.descripcion}</p>
                                 </div>
-                                <button>Editar</button>
-                                <button>Eliminar</button>
+                                <button className="botonEditar"
+                                        //Ojo... no puedo poner el mismo id?
+                                        onClick={(e)=>handleEditar(e)}> Editar 
+                                </button>
+                                <button className="botonEliminar"
+                                        id={gato.id}
+                                        onClick={(e)=>handleEliminar(e)}>Eliminar
+                                </button>
                             </div>
                         </div>
                     </div>
                 ))
             }
+           
         </>
-
+         :
+         <div className="exitoEliminado">
+         <p>Eliminado!</p>
+         </div>}
+    </>
     )
 }
 export default Card;
